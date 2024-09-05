b1696a2f989dc34e091c0e50b469ee1d270a5023
proa83838 <proandthe32@gmail.com>
2024-09-05 22:11:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,11 +39,11 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/nova_updats")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/PhoenixXsupport")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/about_falco")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Nexus_Chat_Group")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
-AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
+AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
 
 
 # Get this credentials from https://developer.spotify.com/dashboard
